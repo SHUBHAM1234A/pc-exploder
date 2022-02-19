28bcf1df07e47e023ebe2e720759a45ff9a888e9
Shubham Kumar Pandey <74237720+SHUBHAM1234A@users.noreply.github.com>
2022-02-19 13:35:45 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "fmt"
+    "strings"
+)
+
+func main() {
+    a := []string{}
+    a = append(a, "h")
+    for i := 0; i <= 1000000000000000000; i++ {
+        a = append(a, "m")
+    }
+    fmt.Println(strings.Join(a, ""))
+}
